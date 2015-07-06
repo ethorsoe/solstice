dc8b179b61ee2d368a9f772871ea2b33a23221ca
Emil Karlson <jekarlson@gmail.com>
2015-07-06 14:13:15 +0300

    refactor main.c

--- main.c
+++ main.c
@@ -69,22 +69,68 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 	return 0;
 }
 
-static struct argp argp = { options, parse_opt, "MOUNT_POINT", "Deduplicate a file system", 0, 0, 0};
 
-int main(int argc, char **argv) {
+enum filtertype {
+	DEDUP_FILTERTYPE_ZEROFIND,
+	DEDUP_FILTERTYPE_SAIS,
+};
+
+static void run_filter(int atfd, enum filtertype type, struct arguments *arguments) {
 	uint32_t *inmem=NULL;
 	uint64_t *checkoffs=NULL;
 	uint64_t *checkinds=NULL;
 	uint64_t *dedups=NULL;
+	int64_t deduplen;
 	int ret=0;
 	time_t prevtime=time(NULL);
-	
+	int64_t generation=btrfs_get_generation(atfd);
+	int64_t metalen = do_search(atfd, &inmem, &checkoffs, &checkinds);
+	if (0>metalen) {
+		fprintf(stderr, "Search failed with %s\n", strerror(-metalen));
+		exit(EXIT_FAILURE);
+	}
+	assert(INT_MAX>metalen);
+	printf("Search done %lu checksums in %lu pieces, took %lu s\n", checkinds[metalen],metalen, (uint64_t)(time(NULL)-prevtime));
+	prevtime=time(NULL);
+
+	switch (type) {
+		case DEDUP_FILTERTYPE_ZEROFIND:
+			deduplen = find_zeros(inmem, checkoffs, checkinds, &dedups, metalen, arguments->minextlen);
+			break;
+		case DEDUP_FILTERTYPE_SAIS:
+			deduplen = do_sais(inmem, checkoffs, checkinds, &dedups, metalen, arguments->minsumsize, arguments->minextlen);
+			break;
+		default:
+			abort();
+	}
+
+	assert(0<=deduplen);
+	printf("Dedup filter creation done %lu candidates, took %lu s\n", deduplen, (uint64_t)(time(NULL)-prevtime));
+	prevtime=time(NULL);
+	free(inmem);
+	free(checkinds);
+	free(checkoffs);
+
+	ret=do_dedups(atfd, dedups, deduplen, arguments->rtable_size, generation);
+	assert(0 <= ret);
+	printf("Filter execution done, took %lu s, %lu root cache misses\n", (uint64_t)(time(NULL)-prevtime), rtable_destroy());
+	free(dedups);
+#ifdef DEDUP_DEBUG_LINK_SRCFILE
+	ret=unlinkat(atfd, DEDUP_DEBUG_LINK_SRCFILE_NAME, 0);
+	assert(0<=ret);
+#endif
+	ret=btrfs_syncfs(atfd);
+	assert(0<=ret);
+}
+
+static struct argp argp = { options, parse_opt, "MOUNT_POINT", "Deduplicate a file system", 0, 0, 0};
+
+int main(int argc, char **argv) {
 	struct arguments arguments;
 	memset(&arguments,0,sizeof(arguments));
 	arguments.minsumsize=2;
 	arguments.minextlen=4;
 	arguments.rtable_size=1024;
-	
 	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
 	int atfd=open(arguments.mount_point, O_RDONLY);
@@ -92,59 +138,13 @@ int main(int argc, char **argv) {
 		perror("Unable to open mount point");
 		exit(EXIT_FAILURE);
 	}
-	ret=btrfs_syncfs(atfd);
+	int ret=btrfs_syncfs(atfd);
 	assert(0<=ret);
 
-	int64_t generation=btrfs_get_generation(atfd);
-	int64_t metalen = do_search(atfd, &inmem, &checkoffs, &checkinds);
-	if (0>metalen) {
-		fprintf(stderr, "Search failed with %s\n", strerror(-metalen));
-		return EXIT_FAILURE;
-	}
-	assert(INT_MAX>metalen);
-	printf("Search done %lu checksums in %lu pieces, took %lu s\n", checkinds[metalen],metalen, (uint64_t)(time(NULL)-prevtime));
-	prevtime=time(NULL);
-	int64_t deduplen = find_zeros(inmem, checkoffs, checkinds, &dedups, metalen, arguments.minextlen);
-	assert(0<=deduplen);
-	printf("Dedup zerofind done %lu candidates, took %lu s\n", deduplen, (uint64_t)(time(NULL)-prevtime));
-	prevtime=time(NULL);
-	free(inmem);
-	free(checkinds);
-	free(checkoffs);
+	run_filter(atfd, DEDUP_FILTERTYPE_ZEROFIND, &arguments);
+	run_filter(atfd, DEDUP_FILTERTYPE_ZEROFIND, &arguments);
+	run_filter(atfd, DEDUP_FILTERTYPE_SAIS, &arguments);
 
-	ret=do_dedups(atfd, dedups, deduplen, arguments.rtable_size, generation);
-	assert(0 <= ret);
-	printf("Hole punch done, took %lu s, %lu root cache misses\n", (uint64_t)(time(NULL)-prevtime), rtable_destroy());
-	free(dedups);
-#ifdef DEDUP_DEBUG_LINK_SRCFILE
-	ret=renameat(atfd, DEDUP_DEBUG_LINK_SRCFILE_NAME, atfd, DEDUP_DEBUG_LINK_SRCFILE_NAME "-zeros");
-	assert(0<=ret);
-#endif
-	ret=btrfs_syncfs(atfd);
-	assert(0<=ret);
-
-	generation=btrfs_get_generation(atfd);
-	metalen = do_search(atfd, &inmem, &checkoffs, &checkinds);
-	if (0>metalen) {
-		fprintf(stderr, "Search failed with %s\n", strerror(-metalen));
-		return EXIT_FAILURE;
-	}
-	assert(INT_MAX>metalen);
-	printf("Search done %lu checksums in %lu pieces, took %lu s\n", checkinds[metalen],metalen, (uint64_t)(time(NULL)-prevtime));
-	prevtime=time(NULL);
-	deduplen = do_sais(inmem, checkoffs, checkinds, &dedups, metalen, arguments.minsumsize, arguments.minextlen);
-	assert(0<=deduplen);
-	printf("Dedup sort done %lu candidates, took %lu s\n", deduplen, (uint64_t)(time(NULL)-prevtime));
-	prevtime=time(NULL);
-	free(inmem);
-	free(checkinds);
-	free(checkoffs);
-
-	ret=do_dedups(atfd, dedups, deduplen, arguments.rtable_size, generation);
-	assert(0 <= ret);
-	printf("Dedup done, took %lu s, %lu root cache misses\n", (uint64_t)(time(NULL)-prevtime), rtable_destroy());
-	free(dedups);
 	close(atfd);
-
 	return !!ret;
 }
